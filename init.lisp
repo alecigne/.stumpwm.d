ba8af066534438233bb4de8afb03f6b4345fe8e7
Anthony Le Cigne <dev@lecigne.net>
2026-03-07 18:23:37 +0100

    Refactor config

--- init.lisp
+++ init.lisp
@@ -1,198 +1,177 @@
-(in-package :stumpwm)
+(in-package #:stumpwm)
 
-(require :swank)
+;; * Meta
+;; Lisp stuff that helps playing with StumpWM itself but doesn't change the
+;; experience yet.
 
-(swank-loader:init)
+;; ** Dependencies
 
-(defcommand swank () ()
-  (setf *top-level-error-action* :break)
-  (swank:create-server :port 4005
-                       :style swank:*communication-style*
-                       :dont-close t))
+;; An ASDF system might be cool in the future, overkill for now.
+(ql:quickload '(:alexandria :slynk) :silent t)
 
-(swank)
+;; ** Helpers
 
-(set-module-dir "~/.stumpwm.d/contrib")
+(defun sh (cmd) (run-shell-command cmd))
 
-(loop for module in '("battery-portable"
-                      "cpu"
-                      "mem"
-                      "surfraw"
-                      "ttf-fonts")
-   do (load-module module))
+(defun sanitize-string (string)
+  (string-trim
+   '(#\Space #\Newline #\Backspace #\Tab #\Linefeed #\Page #\Return #\Rubout)
+   string))
 
-(defcommand create-default-groups () ()
-  (grename "main")
-  (gnewbg-float "flt"))
+(defun sh/out (cmd) (sanitize-string (run-shell-command cmd t)))
 
-(when *initializing*
-  (create-default-groups))
+(defmacro defkeys (map &body bindings)
+  (alexandria:with-gensyms (m)
+    `(let ((,m ,map))
+       ,@(loop for (key command) in bindings
+               collect `(define-key ,m (kbd ,key) ,command)))))
 
-(defcommand shell-command (command) ((:string "sh: "))
-  "Run a shell command and display output to screen.
-  This must be used in a functional side-effects-free style! If a program does not
-  exit of its own accord, StumpWM might hang!"
-  (check-type command string)
-  (echo-string (current-screen)
-               (run-shell-command command t)))
+(defmacro defco (&rest args) `(defcommand ,@args))
 
-(defun show-key-seq (key seq val)
-  (message (print-key-seq (reverse seq))))
+;; ** Slynk
 
-(add-hook *key-press-hook* 'show-key-seq)
+(defvar *slynk-server* nil)
 
-(xft:cache-fonts)
-(set-font (make-instance 'xft:font :family "Source Code Pro" :subfamily "Regular" :size 10))
+(defco slynk-start () ()
+  (unless *slynk-server*
+    (setf *slynk-server* (slynk:create-server :port 4006 :dont-close t)))
+  (message "Slynk server started."))
 
-(setf *screen-mode-line-format*
-      '("%g^n | "
-        "^B%W^b^>"
-        " BAT: %B | %c | %M | "
-        (:eval (stumpwm:run-shell-command "date \"+%F w%V d%w %H:%M\"" t))))
+(defco slynk-stop () ()
+  (when *slynk-server*
+    (slynk:stop-server *slynk-server*)
+    (setf *slynk-server* nil)
+    (message "Slynk server stopped.")))
 
-(when (not (head-mode-line (current-head)))
-  (toggle-mode-line (current-screen) (current-head)))
+(defun stop-slynk-on-exit () (ignore-errors (slynk-stop)))
 
-(setf *startup-message* "^5Lisp ^2^bsystem operational.")
+(pushnew 'stop-slynk-on-exit *quit-hook*)
+(pushnew 'stop-slynk-on-exit *restart-hook*)
+
+;; ** Modules
+
+;; TODO Do not hardcode this
+(set-module-dir "~/src/stumpwm-contrib/")
+
+;; * Basic customization
+
+(setf *startup-message* "^5Lisp ^2^bsystem operational. Welcome!")
 (setf *suppress-frame-indicator* t)
-
-(setf *mouse-focus-policy* :sloppy)
-
-(setf *timeout-wait* 7)
 (setf *window-name-source* :class)
-(setf *maxsize-border-width* 1)
 (setf *message-window-gravity* :bottom-right)
 (setf *input-window-gravity* :bottom-right)
-
+(setf *mode-line-position* :bottom)
 (setf *grab-pointer-foreground* (xlib:make-color :red 1 :green 0 :blue 0))
 (setf *grab-pointer-background* (lookup-color (current-screen) "DeepSkyBlue"))
 (setf *grab-pointer-character* 24)
 (setf *grab-pointer-character-mask* 24)
+(sh "xsetroot -cursor_name left_ptr")
 
-(defcommand toggle-touchpad () ()
-  "Toggle the laptop touchpad on/off."
-  (let ((state (run-shell-command
-                "synclient -l | grep TouchpadOff | awk '{ print $3 }'"
-                t)))
-    (case (string= (subseq state 0 1) "1")
-      ((t) (progn (shell-command "synclient TouchpadOff=0")
-                  (message "Touchpad is now on")))
-      (otherwise (shell-command "synclient TouchpadOff=1")
-                 (message "Touchpad is now off")))))
+;; * Appearance
 
-(defcommand gnome-terminal () ()
-  "Run or raise gnome-terminal."
-  (run-or-raise "dbus-launch gnome-terminal" '(:class "gnome-terminal")))
+(load-module "ttf-fonts")
+(xft:cache-fonts)
+(set-font (make-instance 'xft:font
+                         :family "JetBrains Mono"
+                         :subfamily "Regular"
+                         :size 12
+                         :antialias t))
 
-(defcommand firefox () ()
-  "Run or raise Firefox."
-  (run-or-raise "firefox" '(:class "Firefox")))
+;; * Applications
 
-(defcommand wifi-list () ()
-  "List of available wifi networks."
-  (shell-command "nmcli dev wifi list"))
+;; ** Usual suspects
 
-(defvar *redshift-color-temperature* 6500
-  "Current screen color temperature.")
+(defco firefox () ()
+  (run-or-raise "firefox-esr" '(:class "Firefox"))
+  (message "Opening Firefox..."))
 
-(defcommand redshift-change-color-temperature (amount)
-    ((:number "Amount: "))
-  "Changes the screen color temperature by AMOUNT."
-  (incf *redshift-color-temperature* amount)
-  (run-shell-command (format nil "redshift -O ~a" *redshift-color-temperature*))
-  (message (format nil "Screen color temperature is now ~a" *redshift-color-temperature*)))
+(defco rofi () () (sh "rofi -show combi"))
+(defco alacritty () () (sh "alacritty"))
 
-(defcommand redshift-reset-color-temperature () ()
-  "Reset the screen color temperature to 6500."
-  (redshift-change-color-temperature (- 6500 *redshift-color-temperature*)))
+;; ** xsecurelock
 
-(defcommand translate-selection () ()
-  (run-shell-command
-   (format nil "firefox wordreference.com/enfr/~a" (get-x-selection))))
+(defco lock-screen () ()
+  (sh "XSECURELOCK_PASSWORD_PROMPT=asterisks xsecurelock"))
 
-(defcommand print-screen-area (filename) ((:string "filename: "))
-  (run-shell-command (format nil "import /home/alc/tmp/~a" filename)))
+;; ** Redshift
 
-(defcommand show-uptime () ()
-  "Show current uptime."
-  (shell-command "uptime"))
+(defparameter *redshift-min* 4500)
+(defparameter *redshift-max* 6500)
+(defparameter *redshift-step* 500)
+(defvar *redshift-current* nil)
 
-(defmacro defkeys (map &rest couples)
-  `(if ,map
-       (progn ,@(loop for i in couples collect
-                     `(define-key ,map (kbd ,(first i)) ,(second i))))
-       (setf ,map
-             (let ((map (make-sparse-keymap)))
-               ,@(loop for i in couples collect
-                      `(define-key map (kbd ,(first i)) ,(second i)))
-               map))))
+(defun redshift-message (kelvin)
+  (message "Color temperature is now ~DK." kelvin))
 
-(defkeys *top-map*
-  ("s-TAB" "pull-hidden-other")
-  ("s-i" "iresize")
-  ("s-k" "delete")
-  ("s-m" "gmove")
-  ("s-n" "next")
-  ("s-p" "prev")
-  ("s-s" "hsplit")
-  ("s-S" "vsplit")
-  ("s-o" "only")
-  ("s-C-n" "number")
-  ("s-ampersand" "pull 1")
-  ("s-eacute" "pull 2")
-  ("s-quotedbl" "pull 3")
-  ("s-apostrophe" "pull 4")
-  ("s-parenleft" "pull 5")
-  ("s-minus" "pull 6")
-  ("s-egrave" "pull 7")
-  ("s-underscore" "pull 8")
-  ("s-ccedilla" "pull 9")
-  ("s-agrave" "pull 0")
-  )
+(defco redshift-set (kelvin) ((:number "Kelvin: "))
+  (let ((k (alexandria:clamp (round kelvin) *redshift-min* *redshift-max*)))
+    (setf *redshift-current* k)
+    (sh (format nil "redshift -PO ~D" k))
+    (redshift-message k)
+    k))
 
-(defkeys *top-map*
-  ("s-C" "gnew")
-  ("s-C-C" "gnew-float")
-  ("s-K" "gkill")
-  ("s-N" "gnext")
-  ("s-P" "gprev")
-  ("s-ISO_Left_Tab" "gother") ; Super + Shift + Tab
-  ("s-1" "gselect 1")
-  ("s-2" "gselect 2")
-  ("s-3" "gselect 3")
-  ("s-4" "gselect 4")
-  ("s-5" "gselect 5")
-  ("s-6" "gselect 6")
-  ("s-7" "gselect 7")
-  ("s-8" "gselect 8")
-  ("s-9" "gselect 9")
-  )
+(defun redshift-shift (delta)
+  (redshift-set (+ (or *redshift-current* *redshift-max*) delta)))
+
+(defco redshift-reset () ()
+  (redshift-set *redshift-max*))
+
+(defco redshift-warmer (&optional (step *redshift-step*)) ()
+  (redshift-shift (- step)))
+
+(defco redshift-cooler (&optional (step *redshift-step*)) ()
+  (redshift-shift step))
+
+(defco redshift-night () ()
+  (redshift-set *redshift-min*))
+
+;; ** NordVPN
+
+(defun nord-status ()
+  (sh/out "nordvpn status"))
+
+(defco nord-connect (&optional target) ((:string "Target: "))
+  (if target
+      (sh/out (format nil "nordvpn connect ~A" target))
+      (sh/out "nordvpn connect france")))
+
+(defun nord-disconnect ()
+  (sh "nordvpn disconnect"))
+
+;; * Keyboard
+;; Low-level customization of the keyboard, and keybindings for applications.
+
+(defun load-xmodmap ()
+  (let ((xmodmap-file (merge-pathnames ".Xmodmap" (user-homedir-pathname))))
+    (when (probe-file xmodmap-file)
+      (sh
+       (format nil "setxkbmap -layout fr -variant latin9 && xmodmap ~A"
+               (namestring xmodmap-file))))))
+
+(pushnew 'load-xmodmap *start-hook*)
+(pushnew 'load-xmodmap *restart-hook*)
+
+(set-prefix-key (kbd "s-c"))
 
 (defkeys *top-map*
-  ("s-F4" "redshift-change-color-temperature -250")
-  ("s-F5" "redshift-change-color-temperature +250"))
-
-(defkeys *top-map*
-  ("s-SPC" "gnome-terminal")
-  ("s-!" "exec")
-  ("s-;" "colon")
-  ("s-:" "eval")
-  ("s-b" "banish")
-  ("s-z" "mode-line")
-  ("s-C-n" "number")
-  )
-
-(set-prefix-key (kbd "s-RET"))
+  ("s-j" "next")
+  ("s-k" "prev")
+  ("s-RET" "rofi")
+  ("s-SPC" "alacritty"))
 
 (defkeys *root-map*
-  ("a" '*applications-map*))
+  ("l" "lock-screen")
+  ("m" "mode-line"))
 
-(defvar *applications-map* nil
-  "Applications-related keybindings.")
+;; * Emacs config
 
-(defkeys *applications-map*
-  ("RET" "gnome-terminal")
-  ("e" "emacs")
-  ("f" "firefox")
-  ("F" "firefox-no-remote")
-  )
+;;; Local Variables:
+;;; eval: (progn
+;;;         (put 'defcommand 'lisp-indent-function 'defun)
+;;;         (put 'defco 'lisp-indent-function 'defun)
+;;;         (font-lock-add-keywords
+;;;          nil
+;;;          '(("(\\(defcommand\\|defco\\)\\s-+\\(\\(?:\\sw\\|\\s_\\)+\\)"
+;;;             (1 font-lock-keyword-face)
+;;;             (2 font-lock-function-name-face)))))
+;;; End:
